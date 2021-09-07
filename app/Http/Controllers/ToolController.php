23af85a2f0b61e023deb2449472f12c2aff169d5
thuynga2606 <76727531+thuynga2606@users.noreply.github.com>
2021-09-07 16:09:07 +0700

    phantrang

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -144,8 +144,10 @@ class ToolController extends Controller
         ->join('loaithietbi','loaithietbi.ID_loai','=','thietbi.ID_loai')->join('phong','phong.ID_phong','=','thietbi.ID_phong')->join('tang','tang.ID_tang','=','thietbi.ID_tang')
         ->select('phong.ID_phong','tang.ID_tang','loaithietbi.ID_loai','thietbi.name','thietbi.soluong','thietbi.ngaynhap','thietbi.tinhtrang')
         ->where('name','LIKE','%'.$keyword.'%')
-        ->get();
+        ->paginate(1);
         return view('trangthietbi.trangthietbi',['trangthietbi'=>$trangthietbi]);
+        
+       
     }
 
 
@@ -177,6 +179,7 @@ class ToolController extends Controller
         }else{
         return redirect('/trangthietbi');
         }
+
     }
    
 }
